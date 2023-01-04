b46b3766152f0bb129e6d1277c5ddaf16926a73d
pkh <toby369369@naver.com>
2023-01-04 22:06:36 +0900

    :construction: 프로젝트 생성

---
+++ EventApp/EventApp/EventAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventAppApp.swift
+//  EventApp
+//
+//  Created by Kyoon Ho Park on 2023/01/04.
+//
+
+import SwiftUI
+
+@main
+struct EventAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
